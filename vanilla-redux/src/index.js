ddecd4384e6402d91878f3ce02c979ad445523fc
seungmai <76721552+seungmai@users.noreply.github.com>
2022-02-16 12:39:03 +0900

    리덕스 상태관리

--- vanilla-redux/src/index.js
+++ vanilla-redux/src/index.js
@@ -4,14 +4,17 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
+const ADD = "ADD";
+const MINUS = "MINUS";
+
 const countModifier = (count = 0, action) => {
-  console.log(count, action);
-  if (action.type === "ADD") {
-    return count + 1;
-  } else if (action.type === "MINUS") {
-    return count - 1;
-  } else {
-    return count;
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
   }
 };
 const countStore = createStore(countModifier);
